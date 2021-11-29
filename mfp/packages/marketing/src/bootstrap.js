9ba36b54f7b17d8e05fe921b826debef393724a2
LeeTrent <lee.trent.1@gmail.com>
2021-11-29 12:55:04 -0600

    Added React functionality provided by instructor to Marketing sub-project

--- mfp/packages/marketing/src/bootstrap.js
+++ mfp/packages/marketing/src/bootstrap.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import App from './App';
 ///////////////////////////////////////////////
 // Mount function to start up this application:
 ///////////////////////////////////////////////
 const mount = (el) => {
-    ReactDOM.render(el);
+    ReactDOM.render(<App />, el);
 };
 
 //////////////////////////////////////////////////////////
