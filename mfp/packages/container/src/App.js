05b5761607973e792d70291e49e725f8e291c1f6
LeeTrent <lee.trent.1@gmail.com>
2021-11-29 20:52:42 -0600

    Wired up container and marketing sub-applications

---
+++ mfp/packages/container/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { mount } from 'marketing/MarketingApp';
+
+console.log("[container][App] => (mount): ", mount);
+
+
+export default () => {
+    return <h1>Container</h1>
+};
